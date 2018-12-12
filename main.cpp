833253a42f3e8a80b8e6693759bee403cf76776d
kubagazda <33226707+kubagazda@users.noreply.github.com>
2018-12-12 23:24:50 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,27 @@
 #include <iostream>
 #include <fstream>
 
+using namespace std;
+
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
+    fstream plikWejsciowy;
+    plikWejsciowy.open("a.txt", ios::in);
+    if(plikWejsciowy.good())
+    {
+        cout << "Plik zostal otwarty" << endl;
+        string wiersz;
+        getline(plikWejsciowy, wiersz);
+        plikWejsciowy.close();
+        unsigned int liczbaZnakow = wiersz.size();
+        fstream plikWyjsciowy;
+        plikWyjsciowy.open("b.txt", ios::out);
+        for(unsigned int i = liczbaZnakow; i > 0; i--)
+        {
+            plikWyjsciowy << wiersz.at(i-1);
+        }
+        plikWyjsciowy.close();
+    } else cout << "Dostep do pliku zostal zabroniony lub plik nie istnieje!" << endl;
+
     return 0;
 }
